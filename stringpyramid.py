544e1722154fbf28c955cd82970c8a842a4f453a
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:37:48 -0400

    Add files via upload

---
+++ stringpyramid.py
@@ -0,0 +1,14 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#Mar 8, 2024
+
+pyramid = input("Enter a string: ")
+
+for i in range(len(pyramid)):
+    print(pyramid[:i])
+
+for i in range(len(pyramid)):
+    print(pyramid[i:])
+
+
+print("Thank you for using my program!")
