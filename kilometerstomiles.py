54b0edfc5df852aee0ca90993b1d71973ec1297c
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:45:08 -0400

    Add files via upload

---
+++ kilometerstomiles.py
@@ -0,0 +1,15 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#Mar 8, 2024
+
+#converter example program from lab 4
+
+print("Welcome to Joseph's calculator!")
+#1. Input the distance in kilometers (call it km)
+km = float(input('Enter distance in kilometers: '))
+#2. Calculate the distance in miles as 0.621371*km
+miles = 0.621371*km
+#3. Output miles
+print("The miles are", miles)
+
+
