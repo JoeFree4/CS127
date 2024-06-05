b7818f106027957d6ebf38df8341059f8bd87efb
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:18:18 -0400

    Add files via upload

---
+++ fivepointedstar.py
@@ -0,0 +1,15 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#feb 8, 2024
+
+#this program uses the turtle function to create a flower
+
+import turtle #imports turtle program
+
+james = turtle.Turtle()  #gives turtle varible name
+
+for i in range(5):     #Draws the star
+    james.forward(100)
+    james.left(144)
+
+wn.exitonclick()
