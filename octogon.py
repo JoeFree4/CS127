c9946e9d16225bad08523462ff955182d19be0d5
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:14:07 -0400

    Add files via upload

---
+++ octogon.py
@@ -0,0 +1,14 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+
+import turtle
+
+taylor = turtle.Turtle()
+taylor.color("black")
+taylor.shape("turtle")
+
+for i in range(8):
+    taylor.forward(100)
+    taylor.left(360/8)
+#this program draws an octagon
+turtle.done()
