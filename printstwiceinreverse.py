f3330149e52bad1db25b308e5b17f577c15e86a2
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:35:57 -0400

    Add files via upload

---
+++ printstwiceinreverse.py
@@ -0,0 +1,10 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#Mar 4, 2024
+
+reverse = input("Enter a message: ")
+
+reversemessage = reverse[::-1]
+
+for two in reversemessage:
+    print(two, two)
