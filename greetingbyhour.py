436c3ade1eed7d9bc923985453da9e32e98c17ab
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:56:03 -0400

    Add files via upload

---
+++ greetingbyhour.py
@@ -0,0 +1,13 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#Mar 30,2024
+
+hour = int(input("Enter hour (in 24 hour time): "))
+
+
+if hour < 12:
+    print("Good Morning")
+elif hour < 17:
+    print("Good Afternoon")
+else:
+    print("Good Evening")
