4a89165ef8f1cc202cd4d480c0d2509420591c0d
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:15:22 -0400

    Add files via upload

---
+++ flowerbyturtle.py
@@ -0,0 +1,20 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#feb 8, 2024
+
+#this program uses the turtle function to create a flower
+
+import turtle
+
+james = turtle.Turtle()  #gives turtle varible name
+
+for i in range(36):    #moves turtle 36 times
+    james.forward(100)
+    james.left(145)
+    james.forward(10)
+    james.right(90)
+    james.forward(10)
+    james.left(135)
+    james.forward(100)
+
+wn.exitonclick()
