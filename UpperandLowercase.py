92932740297973a389173221e9c63e89a5b1cb64
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 19:20:47 -0400

    Add files via upload

---
+++ UpperandLowercase.py
@@ -0,0 +1,11 @@
+#Joseph Freeman
+#joseph.freeman41@myhunter.cuny.edu
+#February 14, 2024
+
+#this program prints and Upper and Lower case phrase
+
+phrase = input("Enter a phrase: ")
+
+print(phrase)
+print(phrase.upper())
+print(phrase.lower())
