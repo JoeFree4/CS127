2c3ce11d8780d9399f99b849f13707753b07d955
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 20:05:52 -0400

    Add files via upload

---
+++ randomwalk.py
@@ -0,0 +1,15 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#Apr 14,2024
+
+
+import turtle
+import random
+
+trey = turtle.Turtle()
+trey.speed(10)
+
+for i in range(100):
+    trey.forward(10)
+    a = random.randrange(360)
+    trey.right(a)
