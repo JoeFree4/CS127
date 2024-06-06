577d9152a955356f5771849b9039e07f37d479e6
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 20:09:12 -0400

    Add files via upload

---
+++ Hello.cpp
@@ -0,0 +1,19 @@
+//joseph freeman
+//joseph.freeman41@myhunter.cuny.edu
+//Apr 23, 2024
+
+
+
+#include <iostream>
+
+int main() {
+    // Print "Hello, World!" to the screen
+    std::cout << "Hello, World!" << std::endl;
+
+    // Print "Hello, C++!" to the screen
+    std::cout << "Hello, C++!" << std::endl;
+
+    return 0;
+}
+
+
