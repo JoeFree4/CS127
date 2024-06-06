1bf9b85118e246c1c11d774ea2b78c0426657412
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 20:12:26 -0400

    Add files via upload

---
+++ KilometerstoMiles.cpp
@@ -0,0 +1,23 @@
+//joseph freeman
+//joseph.freeman41@myhunter.cuny.edu
+//Apr 23, 2024
+
+
+
+#include <iostream>
+
+int main() {
+    double kilometers;
+    std::cout << "Enter the number of kilometers: ";
+    std::cin >> kilometers;
+
+    double miles = 0.621371 * kilometers;
+    
+    std::cout << "Equivalent miles: " << miles << std::endl;
+
+    return 0;
+}
+
+
+
+
