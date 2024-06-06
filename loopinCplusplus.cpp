0c1cb950e7f533971a0d909d1251351f23ab3b75
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 20:11:36 -0400

    Add files via upload

---
+++ loopinCplusplus.cpp
@@ -0,0 +1,20 @@
+//joseph freeman
+//joseph.freeman41@myhunter.cuny.edu
+//Apr 23, 2024
+
+
+
+#include <iostream>
+
+int main() {
+    int i;   //The index variable for the loop
+
+    //Loop will repeat 10 times:
+    for (i = 0; i < 10; i++) {
+        std::cout << "Mihi cura futuri\n";
+    }
+    return 0;
+}
+
+
+
