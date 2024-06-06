9fdb692bfc8cdae2b7d2218e1a4f2e5aa2692ceb
JoeFree4 <144745717+JoeFree4@users.noreply.github.com>
2024-06-05 20:06:34 -0400

    Add files via upload

---
+++ emptystringchecking.py
@@ -0,0 +1,15 @@
+#joseph freeman
+#joseph.freeman41@myhunter.cuny.edu
+#Apr 14,2024
+
+def main():
+    while True:
+        enterString = input("Enter a string: ")
+        if enterString != "":
+            print("You entered:", enterString)
+            break
+        else:
+            print("Please enter a non-empty string.")
+
+if __name__ == "__main__":
+    main()
